d0bee973b1aec33129d687823435b5353dcfc77b
ifilippok <ifilippok@gmail.com>
2014-12-21 23:22:23 +0300

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Calculates inverse matrix and putting it value to cache to avoid 
+##                                                   double computation
 
-## Write a short comment describing this function
+
+## This function return list of functions:
+## set - setting matrix for defind value
+## get - getting valie of matrix
+## setInverseMatr - set value for inverse matrix
+## getInverseMatr - get value of inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invMatr <- NULL
+  set1 <- function(y) {
+    x <<- y
+    invMatr <<- NULL
+  }
+  get1 <- function() x
+  setInverseMatr1 <- function(invMatrix) invMatr <<- invMatrix
+  getInverseMatr1 <- function() invMatr
+  
+  #returning functions list
+  # set1, get1, ... is used to separate returing values from internal values
+  list(set = set1, get = get1,
+       setInverseMatr = setInverseMatr1,
+       getInverseMatr = getInverseMatr1)
 }
 
-
-## Write a short comment describing this function
+## This function calculates inverse matrix and put value to cash. If inversed 
+## value has been already calculated it will received from cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  invMatr <- x$getInverseMatr()
+  if(!is.null(invMatr)) {
+    message("getting cached data")
+    return(invMatr)
+  }
+  matr <- x$get()
+  #inverting matrix
+  invMatr <- solve(matr)
+  x$setInverseMatr(invMatr)
+  #returning iversed matrix
+  invMatr
 }
+
+#checking results: myMatr %*% myInv
